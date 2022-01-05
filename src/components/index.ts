87e6e9054990dcb73117912f612c7f88bb83bde7
Rick Bowman <rickbowman63@gmail.com>
2022-01-05 13:34:36 -0700

    export types

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from "./Button";
+export { default as TextInput } from "./TextInput";
+export { ButtonSize } from "./Button";
+export { TextInputSize, TextInputType } from "./TextInput";
